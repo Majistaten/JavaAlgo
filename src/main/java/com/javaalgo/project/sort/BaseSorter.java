6dbcc9bae72295a7e3e280b9a1db5fd64dfa3a24
ruma2100 <ruma2100@student.miun.se>
2022-06-15 20:32:20 +0200

    BaseSorter auto gui

--- src/main/java/com/javaalgo/project/sort/BaseSorter.java
+++ src/main/java/com/javaalgo/project/sort/BaseSorter.java
@@ -1,19 +1,38 @@
 package com.javaalgo.project.sort;
 
+import com.javaalgo.project.gui.MainWindow;
+
 import java.util.Random;
 
 public abstract class BaseSorter {
-    protected int[] values;
+    private int[] _values;
+    protected MainWindow gui;
+    // Default constructor
     private BaseSorter(){
 
     }
+    // Custom constructor
     private BaseSorter(int[] values){
         this.setArray(values);
     }
     public void setArray(int[] values){
-        this.values = values;
+        this._values = values;
+    }
+    //
+    protected int getIndex(int index){
+        updateGui();
+        return this._values[index];
+    }
+    protected void setIndex(int index, int value){
+        updateGui();
+        this._values[index] = value;
+    }
+    //
+    private void updateGui(){
+        this.gui.updateArray(this._values);
     }
 
+    // Class methods
     static public int[] generateArray(int length, int peak){
         Random r = new Random();
         int[] arr = new int[length];
