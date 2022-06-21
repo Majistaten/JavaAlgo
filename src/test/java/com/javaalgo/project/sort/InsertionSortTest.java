81ca3c6fe73bf4d746b7671343d2952d8f82f71f
toha2102 <toha2102@student.miun.se>
2022-06-21 16:01:44 +0200

    Added abstract base to sort tests

--- src/test/java/com/javaalgo/project/sort/InsertionSortTest.java
+++ src/test/java/com/javaalgo/project/sort/InsertionSortTest.java
@@ -5,19 +5,15 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class InsertionSortTest {
-
-    private final int[] unsorted = {5, 4, 3, 2, 1, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
-    private final int[] sorted = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
-    private InsertionSort insertionSort;
+class InsertionSortTest extends TestBaseSort {
 
     @BeforeEach
     void setUp() {
-        insertionSort = new InsertionSort(unsorted);
+        this.baseSort = new InsertionSort(unsorted);
     }
 
     @Test
     void sort() {
-        assertArrayEquals(sorted, insertionSort.execute());
+        assertArrayEquals(sorted, baseSort.execute());
     }
 }
---
+++ src/test/java/com/javaalgo/project/sort/TestBaseSort.java
@@ -0,0 +1,10 @@
+package com.javaalgo.project.sort;
+
+public abstract class TestBaseSort {
+
+    protected final int[] unsorted = {5, 4, 3, 2, 1, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+    protected final int[] sorted = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+
+    protected BaseSorter baseSort;
+
+}
