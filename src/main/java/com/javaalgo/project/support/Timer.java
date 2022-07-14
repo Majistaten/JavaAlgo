edbd157c8e42a7282b795a0cdea31f796f1cfb92
toha2102 <toha2102@student.miun.se>
2022-07-14 15:13:48 +0200

    Made logging optional.

--- src/main/java/com/javaalgo/project/support/Timer.java
+++ src/main/java/com/javaalgo/project/support/Timer.java
@@ -7,14 +7,16 @@ import static java.time.format.DateTimeFormatter.ofPattern;
  */
 public class Timer {
     Timable searcher;
+    boolean logging;
 
     /**
      * Constructor for Timer.
      *
      * @param searcher the algorithm object to be timed.
      */
-    public Timer(Timable searcher) {
+    public Timer(Timable searcher, boolean logging) {
         this.searcher = searcher;
+        this.logging = logging;
     }
 
     /**
@@ -26,9 +28,18 @@ public class Timer {
         long endTime = System.nanoTime();
         long duration = endTime - startTime;
 
-        // TODO: Remove logging from timer and return the result instead.
+        if (logging) {
+            logResults(result, duration);
+        }
+    }
+
+    private void logResults(int[] result, long duration) {
         String log = String.format("%s %-20s key:%-5s at:%-5s in %s nanoseconds.", java.time.LocalDateTime.now().format(ofPattern("HH:mm:ss")),
                 searcher.getClass().getSimpleName(), result[0], result[1], duration);
         ResultLogger.INSTANCE.info(log);
+    }
+
+    public void setSearcher(Timable searcher) {
+        this.searcher = searcher;
     }
 }
